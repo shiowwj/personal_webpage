709cd05636a0b902a5eeeaa33c4f28df8980c42b
shiowwj <shiowweijun@gmail.com>
2019-06-09 20:13:32 +0800

    updatelnks
    git commit -m updatelnks

--- three.js
+++ three.js
@@ -1,104 +1,146 @@
 console.log('hi three.js')
 //Set up scene, camera and render it
-    var canvas = document.getElementById('canvas');
-    console.log('HELLO', canvas)
+// var canvas = document.getElementById('canvas');
+// console.log('HELLO', canvas)
 
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var scene = new THREE.Scene();
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-    //camera position (towards screen or away from screen)
-    camera.position.z = 10;
+//camera position (towards screen or away from screen)
+camera.position.z = 10;
 
-    var renderer = new THREE.WebGLRenderer({antialias:true});
+var renderer = new THREE.WebGLRenderer({ antialias: true });
 
-    //Setting scene background color
-    renderer.setClearColor("#242424");
+//Setting scene background color
+renderer.setClearColor("#242424");
 
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    console.log("THIS IS ME",renderer.domElement);
+renderer.setSize(window.innerWidth, window.innerHeight);
+console.log("THIS IS ME", renderer.domElement);
 
-    //Attached canvas to DOM element
-    canvas.appendChild( renderer.domElement );
+//Attached canvas to DOM element
+document.body.appendChild(renderer.domElement);
 
-    //scene renders responsively to window size
+//scene renders responsively to window size
 
-    window.addEventListener('resize', ()=>{
-        renderer.setSize(window.innerWidth,window.innerHeight);
-        camera.aspect = window.innerWidth / window.innerHeight;
+window.addEventListener('resize', () => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
 
-        camera.updateProjectionMatrix();
-    })
+    camera.updateProjectionMatrix();
+})
 
-    //(RAYCASTER) used to determine where the mouse is on the canvas
-    var raycaster = new THREE.Raycaster();
-    var mouse = new THREE.Vector2();
+//(RAYCASTER) used to determine where the mouse is on the canvas
+var raycaster = new THREE.Raycaster();
+var mouse = new THREE.Vector2();
 
-    //setting objects and items in the camera view
-    //define material and form
-    var geometry = new THREE.SphereGeometry( 1, 25 ,25);
-    var material = new THREE.MeshLambertMaterial({color: 0xF7F7F7});
-    // var sphere = new THREE.Mesh(geometry, material);
+//setting objects and items in the camera view
+//define material and form
+var geometry = new THREE.SphereGeometry(1, 25, 25);
+var material = new THREE.MeshLambertMaterial({ color: 0xF7F7F7 });
+// var sphere = new THREE.Mesh(geometry, material);
 
-    //z position cant be negative
-    // sphere.position.set(0,0,0);
-    // scene.add(sphere);
+//z position cant be negative
+// sphere.position.set(0,0,0);
+// scene.add(sphere);
 
-    sphereX = -10;
-    for(var i = 0; i < 15 ; i++){
-        var sphere = new THREE.Mesh(geometry, material);
-        sphere.position.x = (Math.random() - 0.5) * 10;
-        sphere.position.y = (Math.random() - 0.5) * 10;
-        sphere.position.z = (Math.random() - 0.5) * 10;
-        scene.add(sphere);
-        sphereX +=1;
+sphereX = -10;
+for (var i = 0; i < 15; i++) {
+    var sphere = new THREE.Mesh(geometry, material);
+    sphere.position.x = (Math.random() - 0.5) * 10;
+    sphere.position.y = (Math.random() - 0.5) * 10;
+    sphere.position.z = (Math.random() - 0.5) * 10;
+    scene.add(sphere);
+    sphereX += 1;
+}
+
+var light = new THREE.PointLight(0xFFFFFF, 1, 1000);
+light.position.set(0, 0, 0);
+scene.add(light);
+
+var light = new THREE.PointLight(0xFFFFFF, 0.4, 1000);
+light.position.set(0, 5, 15);
+scene.add(light);
+
+
+var render = function() {
+    requestAnimationFrame(render);
+
+    renderer.render(scene, camera);
+}
+
+function realNumber() {
+
+    var setNum = Math.random() * 2;
+    var numStatus;
+
+    if (setNum > 1) {
+        numStatus = true;
+    } else {
+        numStatus = false;
     }
 
-    var light = new THREE.PointLight(0xFFFFFF, 1, 1000);
-    light.position.set(0,0,0);
-    scene.add(light);
+    return numStatus;
+}
 
-    var light = new THREE.PointLight(0xFFFFFF, 2, 1000);
-    light.position.set(0,0,25);
-    // scene.add(light);
-
-
-    var render = function(){
-        requestAnimationFrame(render);
-
-        renderer.render(scene,camera);
+function randomNum(num) {
+    var postiveOrNegative = realNumber();
+    var outPut;
+    if (postiveOrNegative) {
+        outPut = Math.random() * num;
+    } else {
+        outPut = -1 * (Math.random() * num);
     }
 
-    function onMouseMove(event){
-        console.log('Something here HELLO');
-        event.preventDefault();
-        console.log('mouse para', mouse)
+    return outPut;
+}
 
-        // mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-        // mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
-        console.log('after mouse'. mouse);
-        raycaster.setFromCamera(mouse, camera);
-        //returns array based on the objecs that have intersected where ever the mouse is
-        console.log("children in scene",scene.children);
-        var intersects = raycaster.intersectObjects(scene.children, true);
-        console.log('array of intersected objs', intersects);
-        for(var i = 0; i < intersects.length; i++){
-            console.log('yes sir',intersects[i].object);
-            intersects[i].object.material.color.set(0xFF0000);
-            intersects[i].object.translateX(1);
-            this.tl = new TimelineMax()
-            this.tl.to(intersects[i].object.scale, 1, {x:4, y:4, ease:Expo.easeOut})
-            this.tl.to(intersects[i].object.position, .5, {x:5, y:5, ease:Expo.easeOut})
-            this.tl.to(intersects[i].object.scale, 1, {x:.3, y:.3, ease:Expo.easeOut})
-            this.tl.to(intersects[i].object.scale, 1, {x:1, y:1, ease:Expo.easeOut})
-            this.tl.to(intersects[i].object.position, .5, {x:5, y:-3, ease:Expo.easeOut})
-        }
+function evenMoreRandom(){
+    var outPut = Math.random()*8;
+    return outPut
+}
+
+function onMouseMove(event) {
+    console.log('click handler HELLO');
+    event.preventDefault();
+    console.log('mouse para', mouse)
+
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+    console.log('after mouse'.mouse);
+    raycaster.setFromCamera(mouse, camera);
+    //returns array based on the objecs that have intersected where ever the mouse is
+    console.log("children in scene", scene.children);
+    var intersects = raycaster.intersectObjects(scene.children, true);
+    console.log('array of intersected objs', intersects);
+    for (var i = 0; i < intersects.length; i++) {
+        console.log('yes sir', intersects[i].object);
+        intersects[i].object.material.color.set(0xFF0000);
+        // intersects[i].object.translateX(1);
+        this.tl = new TimelineMax()
+        //randomize the movement
+        // Math.random()*
+        // this.tl.to(intersects[i].object.scale, 1, {x:Math.random()*4, y:Math.random()*4, ease:Expo.easeOut})
+        this.tl.to(intersects[i].object.position, 1, { x: randomNum(evenMoreRandom()), y: randomNum(evenMoreRandom()), ease: Expo.easeOut })
+        // this.tl.to(intersects[i].object.scale, 1, {x:Math.random()*.3, y:Math.random()*.3, ease:Expo.easeOut})
+        // this.tl.to(intersects[i].object.scale, 1, {x:Math.random()*1, y:Math.random()*1, ease:Expo.easeOut})
+        this.tl.to(intersects[i].object.position, 1, { x: randomNum(evenMoreRandom()), y: randomNum(evenMoreRandom()), ease:
+            Expo.easeOut })
+        this.tl.to(intersects[i].object.position, 1, { x: randomNum(evenMoreRandom()), y: randomNum(evenMoreRandom()), ease: Expo.easeOut })
+        this.tl.to(intersects[i].object.position, 1, { x: randomNum(evenMoreRandom()), y: randomNum(evenMoreRandom()), ease: Expo.easeOut })
+        this.tl.to(intersects[i].object.position, 1, { x: randomNum(evenMoreRandom()), y: randomNum(evenMoreRandom()), ease:
+            Expo.easeOut })
+        this.tl.to(intersects[i].object.position, 1, { x: randomNum(evenMoreRandom()), y: randomNum(evenMoreRandom()), ease: Expo.easeOut })
+        this.tl.to(intersects[i].object.position, 1, { x: randomNum(evenMoreRandom()), y: randomNum(evenMoreRandom()), ease: Expo.easeOut })
+        this.tl.to(intersects[i].object.position, 1, { x: randomNum(evenMoreRandom()), y: randomNum(evenMoreRandom()), ease: Expo.easeOut })
     }
+}
 
 
 
 
-    // this.tl.to(this.sphere.scale, 1, {x:2, ease:Expo.easeOut})
 
-    canvas.addEventListener( "click", onMouseMove);
+// this.tl.to(this.sphere.scale, 1, {x:2, ease:Expo.easeOut})
 
-    render();
+window.addEventListener("click", onMouseMove);
+
+render();
